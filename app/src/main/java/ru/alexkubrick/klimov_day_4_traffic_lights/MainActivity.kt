40a6aa0bd86fd9f136fb6445594744f493f01f69
Igor Novikov <igor.novikoww.2000@gmail.com>
2023-04-19 01:20:47 +0700

    resolve issue with on onClick

--- app/src/main/java/ru/alexkubrick/klimov_day_4_traffic_lights/MainActivity.kt
+++ app/src/main/java/ru/alexkubrick/klimov_day_4_traffic_lights/MainActivity.kt
@@ -1,39 +1,44 @@
 package ru.alexkubrick.klimov_day_4_traffic_lights
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rootLayout: ConstraintLayout = findViewById(R.id.root_layout)
-        val textView: TextView = findViewById(R.id.textView)
-        val redButton = findViewById<Button>(R.id.button_red)
+        val redButton: Button = findViewById(R.id.button_red)
         val yellowButton: Button = findViewById(R.id.button_yellow)
         val greenButton: Button = findViewById(R.id.button_green)
 
-        //val clickListener = View.OnClickListener {
-        redButton.setOnClickListener {
-            textView.text = "Красный"
-            rootLayout.setBackgroundColor(resources.getColor(R.color.redColor, null))
-        }
+        redButton.setOnClickListener(viewListener)
+        yellowButton.setOnClickListener(viewListener)
+        greenButton.setOnClickListener(viewListener)
+    }
 
-        yellowButton.setOnClickListener {
-            textView.setText(R.string.Yellow)
-            rootLayout.setBackgroundColor(resources.getColor(R.color.yellowColor))
-        }
+    private val viewListener = View.OnClickListener {
+        val rootLayout: ConstraintLayout = findViewById(R.id.root_layout)
+        val textView: TextView = findViewById(R.id.textView)
 
-        greenButton.setOnClickListener {
-            textView.text = resources.getText(R.string.Green)
-            rootLayout.setBackgroundColor(Color.GREEN)
+        when (it.id){
+            R.id.button_red -> {
+                textView.text = resources.getText(R.string.Red)
+                rootLayout.setBackgroundColor(resources.getColor(R.color.redColor))
+            }
+            R.id.button_yellow -> {
+                textView.text = resources.getText(R.string.Yellow)
+                rootLayout.setBackgroundColor(resources.getColor(R.color.yellowColor))
+            }
+            R.id.button_green -> {
+                textView.text = resources.getText(R.string.Green)
+                rootLayout.setBackgroundColor(resources.getColor(R.color.greenColor))
+            }
         }
     }
 }
